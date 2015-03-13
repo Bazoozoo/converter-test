f7af97e4f1a415c7399a56c05ae06e44d376a561
Alex Palagin <alexpalagin92@gmail.com>
2015-03-13 21:20:37 +0300

    Fixed convert form in which was possibility to set negative amount of currency

--- converter/forms.py
+++ converter/forms.py
@@ -4,6 +4,6 @@ from aggregator.models import CurrencyInfo
 
 
 class ConvertCurrencyForm(forms.Form):
-    amount = forms.FloatField()
+    amount = forms.FloatField(min_value=0.0)
     currency_from = forms.ModelChoiceField(CurrencyInfo.objects.all())
     currency_to = forms.ModelChoiceField(CurrencyInfo.objects.all())
